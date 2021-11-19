b59d225ad8017035176d5d26feceedcb0e1dfcbd
Nakata Nobuyuki <nobu.nakata@gmail.com>
2021-11-19 18:44:32 +0900

    フォルダからzipを自動生成

---
+++ Archiver.ps1
@@ -0,0 +1,18 @@
+﻿$Src = "Build"
+$Dst = "Applications"
+
+foreach ($ch in (Get-ChildItem -Directory $Src)) {
+    $buildRoot = ($ch).FullName
+    $buildName = ($ch).Name
+    $archivePath = Join-Path $Dst "$buildName.zip"
+
+    if ((Test-Path -Path $archivePath) `
+        -and ((Get-Item $archivePath).LastWriteTime -ge ($ch).LastWriteTime)) {
+        continue
+    }
+
+    Write-Host "Compress : $buildName"
+    Compress-Archive -Path $buildRoot -DestinationPath $archivePath -Update
+}
+
+sleep 5
